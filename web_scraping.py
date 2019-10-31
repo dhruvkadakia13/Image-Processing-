074af2aa8a4f0c1cd2d281240b06b66a15c427c5
dhruvkadakia13 <51207534+dhruvkadakia13@users.noreply.github.com>
2019-10-31 13:19:06 +0530

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -4,16 +4,15 @@ pip install requests
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
-response = requests.get('https://electoralsearch.in/Home/VoterInformation')
+response = requests.get('Enter the URL of the site needed to be scraped')
 soup = BeautifulSoup(response.content, 'html.parser')
 posts = soup.findAll('div', class_='one.half')
-# writing to csv file
 with open('election.csv', 'w') as csv_file:
     csv_writer = writer(csv_file)
     headers = ['Title', 'Name']
     csv_writer.writerow(headers)
     for post in posts:
-        title = post.find('tr', class_='border_black_bottom')#.get_text().replace('\n', '')
-        name = post.find('tr', class_='border_black_bottom')#.get_text().replace('\n', '')
+        title = post.find('tr', class_='border_black_bottom').get_text().replace('\n', '')
+        name = post.find('tr', class_='border_black_bottom').get_text().replace('\n', '')
         csv_writer.writerow([title, name])
         
